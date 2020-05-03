dc2ee5d42ce2be16950e5c9c513d2c54cbc3864d
mandar1jn <marijnkneppers@gmail.com>
2020-05-03 06:37:17 +0000

    database fix

--- events/message.js
+++ events/message.js
@@ -7,101 +7,125 @@ defaultJSON = '{}';
 
 module.exports = async (client, message) => {
 
-	guild_info = require(`../databases/guild info/${message.guild.id}.json`);
+    if (fs.existsSync(`./databases/guild info/${message.guild.id}.json`)) {
+    } else {
+        fs.writeFileSync(
+            `./databases/guild info/${message.guild.id}.json`,
+            defaultJSON
+        );
+    }
 
-	if (!guild_info.prefix) {
-		guild_info = {
-			prefix: botconfig.defaultprefix
-		};
-	}
+    if (fs.existsSync(`./databases/xp/xp-${message.guild.id}.json`)) {
+    } else {
+        fs.writeFileSync(
+            `./databases/xp/xp-${message.guild.id}.json`,
+            defaultJSON
+        );
+    }
 
-	fs.writeFile(
-		`./databases/guild info/${message.guild.id}.json`,
-		JSON.stringify(guild_info),
-		err => {
-			if (err) console.log(err);
-		}
-	);
+    if (fs.existsSync(`./databases/munten/munten-${message.guild.id}.json`)) {
+    } else {
+        fs.writeFileSync(
+            `./databases/munten/munten-${message.guild.id}.json`,
+            defaultJSON
+        );
+    }
 
-	if (!blacklistedservers[message.guild.id]) {
-		blacklistedservers[message.guild.id] = false;
-		fs.writeFile(
-			`./databases/blacklistedservers.json`,
-			JSON.stringify(blacklistedservers),
-			err => {
-				if (err) console.log(err);
-			}
-		);
-	}
+    guild_info = require(`../databases/guild info/${message.guild.id}.json`);
 
-	if (blacklistedservers[message.guild.id] === true) {
-		return;
-	}
+    if (!guild_info.prefix) {
+        guild_info = {
+            prefix: botconfig.defaultprefix
+        };
+    }
 
-	if (message.mentions.has(client.user, { ignoreEveryone: true })) {
-		message.channel.send(`The prefix for this server is **${guild_info.prefix}**. Do **${guild_info.prefix}help** for info about all of my commands`);
-	}
+    fs.writeFile(
+        `./databases/guild info/${message.guild.id}.json`,
+        JSON.stringify(guild_info),
+        err => {
+            if (err) console.log(err);
+        }
+    );
 
-	xp = require(`../databases/xp/xp-${message.guild.id}.json`);
-	munten = require(`../databases/munten/munten-${message.guild.id}.json`);
+    if (!blacklistedservers[message.guild.id]) {
+        blacklistedservers[message.guild.id] = false;
+        fs.writeFile(
+            `./databases/blacklistedservers.json`,
+            JSON.stringify(blacklistedservers),
+            err => {
+                if (err) console.log(err);
+            }
+        );
+    }
 
-	const prefix = guild_info.prefix;
+    if (blacklistedservers[message.guild.id] === true) {
+        return;
+    }
 
-	if (message.author.bot) return;
+    if (message.mentions.has(client.user, { ignoreEveryone: true })) {
+        message.channel.send(`The prefix for this server is **${guild_info.prefix}**. Do **${guild_info.prefix}help** for info about all of my commands`);
+    }
 
-	if (!message.guild) return;
+    xp = require(`../databases/xp/xp-${message.guild.id}.json`);
+    munten = require(`../databases/munten/munten-${message.guild.id}.json`);
 
-	if (message.content.startsWith(prefix)) return;
+    const prefix = guild_info.prefix;
 
-	xpAdd = Math.floor(Math.random() * 7) + 8;
+    if (message.author.bot) return;
 
-	if (!xp[message.author.id]) {
-		xp[message.author.id] = {
-			xp: 0,
-			level: 1
-		};
-	}
+    if (!message.guild) return;
 
-	curxp = xp[message.author.id].xp;
-	curlvl = xp[message.author.id].level;
-	nxtLvl = xp[message.author.id].level * 300 * 1.2;
-	xp[message.author.id].xp = curxp + xpAdd;
-	if (nxtLvl <= xp[message.author.id].xp) {
-		xp[message.author.id].level = curlvl + 1;
-		lvlup = new Discord.MessageEmbed()
-			.setTitle('Level Up!')
-			.setColor('ffd000')
-			.addField('New Level', curlvl + 1);
+    if (message.content.startsWith(prefix)) return;
 
-		message.channel.send(lvlup);
-	}
-	fs.writeFile(
-		`./databases/xp/xp-${message.guild.id}.json`,
-		JSON.stringify(xp),
-		err => {
-			if (err) console.log(err);
-		}
-	);
+    xpAdd = Math.floor(Math.random() * 7) + 8;
 
-	if (!munten[message.author.id]) {
-		munten[message.author.id] = {
-			munten: 0,
-			volgende_munt: 5
-		};
-	}
+    if (!xp[message.author.id]) {
+        xp[message.author.id] = {
+            xp: 0,
+            level: 1
+        };
+    }
 
-	munten[message.author.id].volgende_munt -= 1;
+    curxp = xp[message.author.id].xp;
+    curlvl = xp[message.author.id].level;
+    nxtLvl = xp[message.author.id].level * 300 * 1.2;
+    xp[message.author.id].xp = curxp + xpAdd;
+    if (nxtLvl <= xp[message.author.id].xp) {
+        xp[message.author.id].level = curlvl + 1;
+        lvlup = new Discord.MessageEmbed()
+            .setTitle('Level Up!')
+            .setColor('ffd000')
+            .addField('New Level', curlvl + 1);
 
-	if (munten[message.author.id].volgende_munt <= 0) {
-		munten[message.author.id].munten += 1;
-		munten[message.author.id].volgende_munt += 5;
-	}
+        message.channel.send(lvlup);
+    }
+    fs.writeFile(
+        `./databases/xp/xp-${message.guild.id}.json`,
+        JSON.stringify(xp),
+        err => {
+            if (err) console.log(err);
+        }
+    );
 
-	fs.writeFile(
-		`./databases/munten/munten-${message.guild.id}.json`,
-		JSON.stringify(munten),
-		err => {
-			if (err) console.log(err);
-		}
-	);
+    if (!munten[message.author.id]) {
+        munten[message.author.id] = {
+            munten: 0,
+            volgende_munt: 5
+        };
+    }
+
+    munten[message.author.id].volgende_munt -= 1;
+
+    if (munten[message.author.id].volgende_munt <= 0) {
+        munten[message.author.id].munten += 1;
+        munten[message.author.id].volgende_munt += 5;
+    }
+
+    fs.writeFile(
+        `./databases/munten/munten-${message.guild.id}.json`,
+        JSON.stringify(munten),
+        err => {
+            if (err) console.log(err);
+        }
+    );
 };
