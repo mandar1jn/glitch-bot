b519d5c375057bb85aa1c9aca427076d94714c33
Marijn Kneppers <marijnkneppers@gmail.com>
2020-06-09 10:28:13 +0000

    added a permission check at kick.js

--- src/bot/commands/moderation/setprefix.js
+++ src/bot/commands/moderation/setprefix.js
@@ -1,5 +1,5 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
     name: "setprefix",
@@ -11,13 +11,15 @@ module.exports = {
 
         if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("Sorry, but you don't have the permission to do that")
 
-        if(!args[0]) return message.channel.send("Please also specifie a prefix");
+        if (!args[0]) return message.channel.send("Please also specifie a prefix");
 
         guild_info.prefix = args[0];
 
-       fs.writeFile(path.resolve(`src/bot/databases/guild info/${message.guild.id}.json`), JSON.stringify(guild_info));
+        fs.writeFile(path.resolve(`src/bot/databases/guild info/${message.guild.id}.json`), JSON.stringify(guild_info), function(err) {
+            if (err) console.log('error', err);
+        });
 
-    message.channel.send(`I have changed the prefix to \`\`${args[0]}\`\``)
+        message.channel.send(`I have changed the prefix to \`\`${args[0]}\`\``)
 
     }
 }
