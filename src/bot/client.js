7a6345a1dbf63680ec50ae7ec30d7c95a850c44a
Marijn Kneppers <marijnkneppers@gmail.com>
2020-09-03 12:38:30 +0000

    Gave events a dedicated folder

--- src/bot/client.js
+++ src/bot/client.js
@@ -21,33 +21,33 @@ client.aliases = new Discord.Collection();
     require(path.resolve(`src/bot/handlers/${handler}`))(client);
 });
 
-fs.readdir(path.resolve('src/bot/client-events/'), (err, files) => {
+fs.readdir(path.resolve('src/bot/events/client/'), (err, files) => {
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
-        const event = require(path.resolve(`src/bot/client-events/${file}`));
+        const event = require(path.resolve(`src/bot/events/client/${file}`));
         let eventName = file.split('.')[0];
         client.on(eventName, event.bind(null, client));
-        delete require.cache[require.resolve(path.resolve(`src/bot/client-events/${file}`))];
+        delete require.cache[require.resolve(path.resolve(`src/bot/events/client/${file}`))];
     });
 });
 
-fs.readdir(path.resolve('src/bot/dbl-events/'), (err, files) => {
+fs.readdir(path.resolve('src/bot/events/dbl/'), (err, files) => {
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
-        const event = require(path.resolve(`src/bot/dbl-events/${file}`));
+        const event = require(path.resolve(`src/bot/events/dbl/${file}`));
         let eventName = file.split('.')[0];
         dbl.on(eventName, event.bind(null, dbl));
-        delete require.cache[require.resolve(path.resolve(`src/bot/dbl-events/${file}`))];
+        delete require.cache[require.resolve(path.resolve(`src/bot/events/dbl/${file}`))];
     });
 });
 
-fs.readdir(path.resolve('src/bot/dbl-webhook-events/'), (err, files) => {
+fs.readdir(path.resolve('src/bot/events/dbl-webhook/'), (err, files) => {
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
-        const event = require(path.resolve(`src/bot/dbl-webhook-events/${file}`));
+        const event = require(path.resolve(`src/bot/events/dbl-webhook/${file}`));
         let eventName = file.split('.')[0];
         dbl.webhook.on(eventName, event.bind(null, dbl.webhook));
-        delete require.cache[require.resolve(path.resolve(`src/bot/dbl-webhook-events/${file}`))];
+        delete require.cache[require.resolve(path.resolve(`src/bot/events/dbl-webhook/${file}`))];
     });
 });
 
