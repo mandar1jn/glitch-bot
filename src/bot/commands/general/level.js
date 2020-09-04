2b406896d3ea353c3a6ac04272463c0f7b3ce726
Marijn Kneppers <marijnkneppers@gmail.com>
2020-09-04 10:04:15 +0000

    removed unused variable and indented some stuff

--- src/bot/commands/general/level.js
+++ src/bot/commands/general/level.js
@@ -7,7 +7,7 @@ module.exports = {
     description: "shows your level",
     category: "general",
     run: async (client, message) => {
-        if(!message.guild.me.hasPermission("SEND_MESSAGES")){
+        if (!message.guild.me.hasPermission("SEND_MESSAGES")) {
             return;
         }
 
@@ -21,14 +21,13 @@ module.exports = {
         let curxp = xp[message.author.id].xp;
         let curlvl = xp[message.author.id].level;
         let nxtLvlXp = curlvl * 300 * 1.2;
-        let difference = nxtLvlXp - curxp;
 
-let image = canvas.createCanvas(200, 100)
-let ctxAvatar = image.getContext("2d");
-let xpCtx = image.getContext("2d");
-let xpText = curxp + "/" + nxtLvlXp + " xp to level " + curlvl + 1;
-xpCtx.font = '12px "Comic Sans"'
-xpCtx.fillText(xpText, 0, 0)
+        let image = canvas.createCanvas(200, 100)
+        let ctxAvatar = image.getContext("2d");
+        let xpCtx = image.getContext("2d");
+        let xpText = curxp + "/" + nxtLvlXp + " xp to level " + curlvl + 1;
+        xpCtx.font = '12px "Comic Sans"'
+        xpCtx.fillText(xpText, 0, 0)
         let attachment = new Discord.MessageAttachment(image.toBuffer());
 
         message.channel.send(attachment);
