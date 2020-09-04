59421ced5572c602ef7054b989d865e14ccfbf58
Marijn Kneppers <marijnkneppers@gmail.com>
2020-09-04 06:38:33 +0000

    refactored start.js and added comments to everything

--- src/start.js
+++ src/start.js
@@ -1,26 +1,36 @@
+//sets dotenv up
 require('dotenv').config();
-
+//requires path because it is needed to require other files in the project
 const path = require("path");
-process.env.FONTCONFIG_PATH = path.resolve("src/fonts/");
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager(path.resolve('src/bot/client.js'), { token: process.env.TOKEN });
-//const server = require(path.resolve('src/site/server.js'));
+//requires fs to be able to read and edit files
 const fs = require("fs")
 
-manager.on('message', (manager, shard, message) => {
-	console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
-});
+//starts the bot
 
-fs.readdir(path.resolve('src/bot/manager-events/'), (err, files) => {
+//requires the sharmanager from the discord api.
+const { ShardingManager } = require('discord.js');
+
+//creates a manager object with a path to the bot file and the token from the .env file
+const manager = new ShardingManager(path.resolve('src/bot/client.js'), { token: process.env.TOKEN });
+
+//reads the files drom the events folder and links them to the corrensponding events
+fs.readdir(path.resolve('src/bot/events/manager/'), (err, files) => {
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
-        const event = require(path.resolve(`src/bot/manager-events/${file}`));
+        const event = require(path.resolve(`src/bot/events/manager/${file}`));
         let eventName = file.split('.')[0];
         manager.on(eventName, event.bind(null, manager));
-        delete require.cache[require.resolve(path.resolve(`src/bot/manager-events/${file}`))];
+        delete require.cache[require.resolve(path.resolve(`src/bot/events/manager/${file}`))];
     });
 });
 
-//server.listen(process.env.PORT || 8080);
-
+//starts the manager
 manager.spawn();
+
+//starts the website
+
+//requires the server file
+const server = require(path.resolve('src/site/server.js'));
+
+//starts the server
+server.listen(process.env.PORT || 8080);
