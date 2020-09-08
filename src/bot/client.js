36aa952c70730cdd57abb57666480c9d462e8ac8
Marijn Kneppers <marijnkneppers@gmail.com>
2020-09-08 08:40:29 +0000

    removed unused variables

--- src/bot/client.js
+++ src/bot/client.js
@@ -1,7 +1,5 @@
 const path = require("path")
 const Discord = require('discord.js');
-const fs = require('fs');
-const developers = require(path.resolve(`src/bot/databases/developers.json`));
 const client = new Discord.Client({
     shards: 'auto',
     disableMentions: 'everyone',
@@ -11,9 +9,6 @@ const DBL = require('dblapi.js');
 var dbl = new DBL(process.env.TOPAPI, { webhookPort: 5000, webhookAuth: "glitched" }, client);
 
 module.exports = client;
-var prefix = null;
-var guild_info = null;
-const utils = require(path.resolve(`src/utils.js`));
 
 require(path.resolve(`src/bot/handlers/command.js`)).registerCommands(client);
 
