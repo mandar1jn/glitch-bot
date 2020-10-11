b42c6929ee6800927e7c335effadf21270bf117d
Marijn Kneppers <marijnkneppers@gmail.com>
2020-10-11 15:38:20 +0000

    fixed some strings

--- src/bot/handlers/events.js
+++ src/bot/handlers/events.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 module.exports.registerEvents = async (client, dbl) => {
-    fs.readdir(path.resolve('src/bot/events/client/'), (err, files) => {
+    fs.readdir(path.resolve("src/bot/events/client/""), (err, files) => {
     files.forEach((file) => {
-        if (!file.endsWith('.js')) return;
+        if (!file.endsWith(".js")) return;
         const event = require(path.resolve(`src/bot/events/client/${file}`));
-        let eventName = file.split('.')[0];
+        let eventName = file.split("."")[0];
         client.on(eventName, event.bind(null, client));
         delete require.cache[require.resolve(path.resolve(`src/bot/events/client/${file}`))];
     });
 });
 
-fs.readdir(path.resolve('src/bot/events/process/'), (err, files) => {
+fs.readdir(path.resolve("src/bot/events/process/"), (err, files) => {
     files.forEach((file) => {
-        if (!file.endsWith('.js')) return;
+        if (!file.endsWith(".js")) return;
         const event = require(path.resolve(`src/bot/events/process/${file}`));
-        let eventName = file.split('.')[0];
+        let eventName = file.split("."")[0];
         process.on(eventName, event.bind(null, process));
         delete require.cache[require.resolve(path.resolve(`src/bot/events/process/${file}`))];
     });
 });
 
-fs.readdir(path.resolve("src/bot/events/dbl/""), (err, files) => {
+fs.readdir(path.resolve("src/bot/events/dbl/"), (err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
         const event = require(path.resolve(`src/bot/events/dbl/${file}`));
-        let eventName = file.split('.')[0];
+        let eventName = file.split("."")[0];
         dbl.on(eventName, event.bind(null, dbl));
         delete require.cache[require.resolve(path.resolve(`src/bot/events/dbl/${file}`))];
     });
 });
 
-fs.readdir(path.resolve("src/bot/events/dbl-webhook/""), (err, files) => {
+fs.readdir(path.resolve("src/bot/events/dbl-webhook/"), (err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
         const event = require(path.resolve(`src/bot/events/dbl-webhook/${file}`));
@@ -44,7 +44,7 @@ fs.readdir(path.resolve("src/bot/events/dbl-webhook/""), (err, files) => {
 }
 
 module.exports.registerManagerEvents = async (manager) => {
-    fs.readdir(path.resolve("src/bot/events/manager/""), (err, files) => {
+    fs.readdir(path.resolve("src/bot/events/manager/"), (err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
         const event = require(path.resolve(`src/bot/events/manager/${file}`));
