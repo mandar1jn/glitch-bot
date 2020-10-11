1ef6135d985f12f763e61dc0ffb8da221abe6db8
Marijn Kneppers <marijnkneppers@gmail.com>
2020-10-11 14:42:53 +0000

    some issue fixes (more coming)

--- src/bot/commands/general/level.js
+++ src/bot/commands/general/level.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const canvas = require("canvas");
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
             return;
         }
 
-        let xp = require(path.resolve(`src/bot/databases/xp/xp-${messageObject.message.guild.id}.json`));
+        let xp = JSON.parse(fs.readFileSync(path.resolve(`src/bot/databases/xp/xp-${messageObject.message.guild.id}.json`)));
         if (!xp[messageObject.message.author.id]) {
             xp[messageObject.message.author.id] = {
                 xp: 0,
@@ -20,21 +20,22 @@ module.exports = {
             };
         }
         fs.writeFile(
-        path.resolve(`src/bot/databases/xp/xp-${messageObject.message.guild.id}.json`),
-        JSON.stringify(xp), function(err) {
-            if (err) console.log('error', err);
-        });
+            path.resolve(`src/bot/databases/xp/xp-${messageObject.message.guild.id}.json`),
+            JSON.stringify(xp), function(err) {
+                if (err) {
+                    console.log("error", err);
+                }
+            });
         let curxp = xp[messageObject.message.author.id].xp;
         let curlvl = xp[messageObject.message.author.id].level;
         let nxtLvlXp = curlvl * 300 * 1.2;
 
-        let image = canvas.createCanvas(200, 100)
+        let image = canvas.createCanvas(200, 100);
         //uncomment when used
         //let ctxAvatar = image.getContext("2d");
         let xpCtx = image.getContext("2d");
         let xpText = curxp + "/" + nxtLvlXp + " xp to level " + curlvl + 1;
-        xpCtx.font = '12px "Comic Sans"'
-        xpCtx.fillText(xpText, 0, 0)
+        xpCtx.fillText(xpText, 0, 0);
         let attachment = new Discord.MessageAttachment(image.toBuffer());
 
         return messageObject.message.channel.send(attachment);
