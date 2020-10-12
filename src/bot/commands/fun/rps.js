d3192a91e817add0b7b40d58784e1f6bb9b7d90c
Marijn Kneppers <marijnkneppers@gmail.com>
2020-10-12 08:16:01 +0000

    changed some == to ===

--- src/bot/commands/fun/rps.js
+++ src/bot/commands/fun/rps.js
@@ -13,38 +13,38 @@ module.exports = {
 
         var result = options[Math.floor(Math.random() * options.length)];
 
-        if (messageObject.args[0] == "rock") {
-            if (result == "paper") {
+        if (messageObject.args[0] === "rock") {
+            if (result === "paper") {
                 messageObject.message.channel.send(`I have ${result} :notepad_spiral:. I win!`);
             }
-            else if (result == "scissor") {
+            else if (result === "scissor") {
                 messageObject.message.channel.send(`I have ${result} :scissors:. You win!`);
             }
-            else if (result == "rock") {
+            else if (result === "rock") {
                 messageObject.message.channel.send(`I have ${result} :curling_stone:. It is a tie!`);
             }
         }
-        else if (messageObject.args[0] == "paper") {
-            if (result == "paper") {
+        else if (messageObject.args[0] === "paper") {
+            if (result === "paper") {
                 messageObject.message.channel.send(`I have ${result} :notepad_spiral:. It is a tie!`);
             }
-            else if (result == "scissor") {
+            else if (result === "scissor") {
                 messageObject.message.channel.send(`I have ${result} :scissors:. I win!`);
             }
-            else if (result == "rock") {
+            else if (result === "rock") {
                 messageObject.message.channel.send(`I have ${result} :curling_stone:. You win!`);
             }
         }
-        else if (messageObject.args[0] == "scissor") {
-            if (result == "paper") {
+        else if (messageObject.args[0] === "scissor") {
+            if (result === "paper") {
                 messageObject.message.channel.send(`I have ${result} :notepad_spiral:. You win!`);
             }
-            else if (result == "scissor") {
+            else if (result === "scissor") {
                 messageObject.message.channel.send(`I have ${result} :scissors:. It is a tie!`);
             }
-            else if (result == "rock") {
+            else if (result === "rock") {
                 messageObject.message.channel.send(`I have ${result} :curling_stone:. I win!`);
             }
         }
     }
-}
+};
